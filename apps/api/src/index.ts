16d4d3a915132bb6f107c3d31df59371b15ba37a
ruzbyte <arthuraktamirov@gmail.com>
2026-01-06 23:52:27 +0100

    feat: add api base

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,12 +1,9 @@
 import { Elysia } from "elysia";
 import { cors } from "@elysiajs/cors";
+import { openapi } from "@elysiajs/openapi";
+import { container } from "./modules/container/index.ts";
 
-const app = new Elysia()
-  .use(cors())
-  .get("/", () => "Hello from Elysia API")
-  .get("/users", () => [{ id: 1, name: "Alice" }])
-  .listen(8080);
-
+const app = new Elysia().use(cors()).use(openapi()).use(container).listen(8080);
 export type App = typeof app;
 
 console.log(`Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
