e8d4406f989da00980d40894be9bd887d193ba45
LetsDrinkSomeTea <104939409+LetsDrinkSomeTea@users.noreply.github.com>
2026-01-07 23:36:11 +0100

    added logging endpoint (#3)
    * added logging endpoint with query params

--- apps/api/src/modules/container/index.ts
+++ apps/api/src/modules/container/index.ts
@@ -87,6 +87,33 @@ export const container = new Elysia({ prefix: "/container" })
         500: ContainerModel.response,
       },
     }
+  ).get(
+    "/:id/logs",
+    ({ params, query }) => {
+      const logOptions = {
+        stdout: { stdout: true, stderr: false },
+        stderr: { stdout: false, stderr: true },
+        both: { stdout: true, stderr: true }
+      };
+
+      // Fallback auf 'stdout', falls query.type leer oder unbekannt ist
+      const { stdout, stderr } = logOptions[query.type || "stdout"];
+      return ContainerService.getLogs(params.id, stdout, stderr, query.tail);
+    },
+    {
+      params: t.Object({
+        id: t.String(),
+      }),
+      query: t.Object({
+        type: t.Optional(t.Union([t.Literal("stdout"), t.Literal("stderr"), t.Literal("both")], { default: "stdout" })),
+        tail: t.Optional(t.Number({ default: 50 })),
+      }),
+      response: {
+        200: ContainerModel.responseLogs,
+        404: ContainerModel.response,
+        500: ContainerModel.response,
+      },
+    }
   )
   .get(
     "/list",
